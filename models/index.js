583b82224626f43bb91a30eaaa49e132e72e9904
Josh Carter <joshcarter8400@gmail.com>
2020-10-29 19:04:51 -0400

    fixed foreign keys

--- models/index.js
+++ models/index.js
@@ -6,16 +6,23 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "id",
+  foreignKey: "category_id",
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "id",
+  foreignKey: "category_id",
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  through: ProductTag,
+});
+
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+  through: ProductTag,
+});
 module.exports = {
   Product,
   Category,
